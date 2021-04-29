0155b5f03c929af0284f88c7cb8d407840a2e30f
Dasol Park <dasol.park615@gmail.com>
2021-04-29 22:33:10 +0900

    try to make header

--- build/script.js
+++ build/script.js
@@ -1,3 +1,13 @@
 "use strict";
-console.log('hello daily routine');
+var DAYS = ['Sun', 'Mon', 'Tue', 'Wed', 'Thur', 'Fri', 'Sat'];
+function getDay() {
+    return DAYS[new Date().getDay()];
+}
+var Header = function () {
+    return "\n      <ul>\n        " + DAYS.map(function (day) { return "<li>" + day + "</li>"; }).join('') + "\n      </ul>\n    ";
+};
+var headerEl = document.querySelector('#header');
+if (headerEl) {
+    headerEl.innerHTML = Header();
+}
 //# sourceMappingURL=script.js.map
--- src/script.ts
+++ src/script.ts
@@ -1,1 +1,20 @@
-console.log('hello daily routine');
+type Day = 'Sun' | 'Mon' | 'Tue' | 'Wed' | 'Thur' | 'Fri' | 'Sat';
+
+const DAYS: Day[] = ['Sun', 'Mon', 'Tue', 'Wed', 'Thur', 'Fri', 'Sat'];
+
+function getDay(): Day {
+  return DAYS[new Date().getDay()];
+}
+
+const Header = () => {
+  return `
+      <ul>
+        ${DAYS.map(day => `<li>${day}</li>`).join('')}
+      </ul>
+    `
+}
+
+const headerEl = document.querySelector('#header');
+if (headerEl) {
+  headerEl.innerHTML = Header();
+}
